ace8bf190643844083ac3cf22f5195b249b05530
jiangchuan <jiangchuanheglobal@gmail.com>
2015-01-04 05:36:42 -0500

    a general result of events page

---
+++ app/src/main/java/info/jiangchuan/wrca/Event.java
@@ -0,0 +1,74 @@
+package info.jiangchuan.wrca;
+
+/**
+ * Created by jiangchuan on 1/3/15.
+ */
+public class Event {
+    private String title;
+    private String location;
+    private String time;
+    private String description;
+    private String thumbnailUrl;
+    private String contact;
+
+    public Event() {
+
+    }
+    public Event(String title, String location, String time, String description, String thumbnailUrl, String contact) {
+        this.title = title;
+        this.location = location;
+        this.time = time;
+        this.description = description;
+        this.thumbnailUrl = thumbnailUrl;
+        this.contact = contact;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
--- app/src/main/java/info/jiangchuan/wrca/EventsActivity.java
+++ app/src/main/java/info/jiangchuan/wrca/EventsActivity.java
@@ -4,32 +4,152 @@ package info.jiangchuan.wrca;
  * Created by jiangchuan on 1/3/15.
  */
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
-public class EventsActivity extends ListActivity {
+import java.util.List;
+import java.util.ArrayList;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.NetworkResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import android.widget.AbsListView;
+public class EventsActivity extends Activity {
+
+    private static final String TAG = "EventsActivity";
+    private List<Event> eventsList = new ArrayList<Event>();
+    private ListView listView;
+    private CustomListAdapter adapter;
+
+    private int lastItem = 0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+        listView = (ListView) findViewById(R.id.list);
 
-        String[] info = new String[] { "ea","eo","ee",
-                "ei","ed","eh"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row_events, R.id.label, info);
-        setListAdapter(adapter);
+        adapter = new CustomListAdapter(this, eventsList);
+        listView.setAdapter(adapter);
+
+        //pullMoreData();
+        // Creating volley request obj
+        String url = "http://jiangchuan.info/php/index.php?object=events&type=all&cursorPos=" + lastItem+1;
+        JsonArrayRequest eventReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        lastItem += response.length();
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Event movie = new Event();
+                                movie.setTitle(obj.getString("title"));
+                                movie.setThumbnailUrl(obj.getString("thumbnail_url"));
+                                movie.setLocation(obj.getString("location"));
+                                movie.setTime(obj.getString("time"));
+
+                                // adding movie to movies array
+                                eventsList.add(movie);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        });
+        // Adding request to request queue
+        WRCAApplication.getInstance().getRequestQueue().add(eventReq);
+        listView.setOnScrollListener(new ListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view,
+                                             int scrollState) {
+                // Do nothing
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {
+
+                // threshold being indicator if bottom of list is hit
+
+                if (firstVisibleItem == totalItemCount-5) {
+                    pullMoreData();
+                }
+            }
+        });
     }
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        String item = (String) getListAdapter().getItem(position);
-        //Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
-        //Intent intent = new Intent(this, EventsItemActivity.class);
-        //startActivity(intent);
+
+    private void pullMoreData() {
+        // Creating volley request obj
+        String url = "http://jiangchuan.info/php/index.php?object=events&type=all&cursorPos=" + 3;
+        Log.d(TAG, "pull more data");
+        JsonArrayRequest eventReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        lastItem += response.length();
+                        // Parsing json
+                        //Log.d(TAG, Integer.toString(eventsList.size()));
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Event movie = new Event();
+                                movie.setTitle(obj.getString("title"));
+                                movie.setThumbnailUrl(obj.getString("thumbnail_url"));
+                                movie.setLocation(obj.getString("location"));
+                                movie.setTime(obj.getString("time"));
+
+                                // adding movie to movies array
+                                eventsList.add(movie);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        Log.d(TAG, Integer.toString(eventsList.size()));
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.d(TAG, "Error: " + error.toString());
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null)
+                Log.e(TAG, Integer.toString(networkResponse.statusCode));
+            }
+        });
+        // Adding request to request queue
+        WRCAApplication.getInstance().getRequestQueue().add(eventReq);
     }
+
 }
