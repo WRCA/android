b378114d609d8a5bdb81f98dad00360becab2b8e
jiangchuan <jiangchuanheglobal@gmail.com>
2015-01-25 04:07:34 -0500

    add refresh option for notifiction menu

--- app/src/main/java/info/jiangchuan/wrca/PushNotificationActivity.java
+++ app/src/main/java/info/jiangchuan/wrca/PushNotificationActivity.java
@@ -85,7 +85,8 @@ public class PushNotificationActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
+            adapter.notifyDataSetChanged();
             return true;
         }
 
