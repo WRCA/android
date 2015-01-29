df2d8910418e4d25ad56d0c4227c21985bce3155
jiangchuan <jiangchuanheglobal@gmail.com>
2015-01-29 14:55:33 -0500

    add facebook likebutton

--- app/src/main/java/info/jiangchuan/wrca/ui/AboutActivity.java
+++ app/src/main/java/info/jiangchuan/wrca/ui/AboutActivity.java
@@ -1,8 +1,10 @@
 package info.jiangchuan.wrca.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 import com.facebook.Settings;
 import com.facebook.widget.LikeView;
@@ -12,7 +14,7 @@ import info.jiangchuan.wrca.R;
 /**
  * Created by jiangchuan on 1/28/15.
  */
-public class AboutActivity extends ActionBarActivity {
+public class AboutActivity extends ActionBarActivity implements View.OnClickListener{
 
     LikeView likeView;
 
@@ -26,10 +28,21 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
         Settings.sdkInitialize(this);
-        likeView = (LikeView) findViewById(R.id.like_view);
-        likeView.setObjectId("https://www.facebook.com/pages/Willow-Ridge-Civic-Association/167497999970646?ref=hl");
+
+        try {
+            Settings.sdkInitialize(this);
+            likeView = (LikeView) findViewById(R.id.like_view);
+            likeView.setObjectId("https://www.facebook.com/pages/Willow-Ridge-Civic-Association/167497999970646?ref=hl");
+            likeView.setForegroundColor(R.color.color_major);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
